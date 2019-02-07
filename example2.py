4599c2f64530da9d53985dbaa667e4a90562ec04
Andrew Gill <gilamonster.A@gmail.com>
2019-02-07 13:55:09 +0200

    Delete example2.py

--- example2.py
+++
@@ -1,6 +0,0 @@
-# ask the user to input a string
-mystr = input("enter a string: ")
-# find the middle character of the string (round down if the string len is even)
-mid=(len(mystr)-1)//2
-# print out the middle character
-print("the middle character is: "+mystr[mid])
